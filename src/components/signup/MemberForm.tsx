07edf16568066e4eaab9ef84d8e2ed55c2d794e9
yiminwook <grs0416@gmail.com>
2023-05-17 02:47:44 +0900

    Fix: SignUpForm gap and padding

--- src/components/signup/MemberForm.tsx
+++ src/components/signup/MemberForm.tsx
@@ -3,6 +3,7 @@ import { FormEvent, useEffect, useRef } from 'react';
 import signUpForm from '@/components/signup/SignUpForm.module.scss';
 import { useUser } from '@/hooks/useUser';
 import { useNavigate } from 'react-router-dom';
+import { BiSearch } from 'react-icons/bi';
 
 interface MemberFormElements extends HTMLFormControlsCollection {
   email: HTMLInputElement;
@@ -54,16 +55,6 @@ const MemberForm = ({ sendSignUpData, openPostModal, roadAddress }: FormProps) =
       <label htmlFor="name">이름</label>
       <input name="name" type="text" />
 
-      <div className={signUpForm['ageSex']}>
-        <label htmlFor="age">나이</label>
-        <input name="age" type="number" />
-        <label htmlFor="sex">성별</label>
-        <select name="sex">
-          <option value="">선택</option>
-          <option value="man">남성</option>
-          <option value="woman">여성</option>
-        </select>
-      </div>
       <label htmlFor="occupation">직업</label>
       <input name="occupation" type="text" />
 
@@ -78,11 +69,23 @@ const MemberForm = ({ sendSignUpData, openPostModal, roadAddress }: FormProps) =
           onFocus={openPostModal}
         />
         <button type="button" onClick={openPostModal}>
-          주소검색
+          <BiSearch size="1.5rem" color="inherit" />
         </button>
       </div>
-      <label htmlFor="addressDetail">상세주소</label>
+      <label htmlFor="addressDetail">상세</label>
       <input name="addressDetail" type="text" placeholder="상세주소" />
+
+      <div className={signUpForm['ageSex']}>
+        <label htmlFor="age">나이</label>
+        <input name="age" type="number" />
+        <label htmlFor="sex">성별</label>
+        <select name="sex">
+          <option value="">선택</option>
+          <option value="man">남성</option>
+          <option value="woman">여성</option>
+        </select>
+      </div>
+
       <div className={signUpForm['footer']}>
         <button type="submit">제출</button>
       </div>
