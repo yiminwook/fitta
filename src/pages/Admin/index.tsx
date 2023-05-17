686fdf4e55ccecd5bb9d26a0249d89a12d6f06a7
yiminwook <grs0416@gmail.com>
2023-05-18 03:53:43 +0900

    Add: loading component styling

--- src/pages/Admin/index.tsx
+++ src/pages/Admin/index.tsx
@@ -1,3 +1,4 @@
+import Loading from '@/components/common/Loading';
 import Head from '@/components/layout/Head';
 import Modal from '@/components/layout/Modal';
 import { useState } from 'react';
@@ -10,9 +11,19 @@ const Admin = () => {
       <section>Admin</section>
       <br />
       <button onClick={() => setIsShow(true)}>모달 오픈 버튼</button>
+      <br />
+      <section>
+        로딩 컴포넌트 예시
+        <div style={{ border: '1px solid black', height: '10rem' }}>
+          <Loading style={{ height: '10rem' }} />
+        </div>
+      </section>
       {isShow ? (
         <Modal title="테스트" onClose={() => setIsShow(false)}>
-          <div>모달</div>
+          <div style={{ height: '10rem' }}>모달예시</div>
+          <div>
+            <button style={{ margin: 'auto' }}>확인</button>
+          </div>
         </Modal>
       ) : null}
     </>
