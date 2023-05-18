e83d2927f7ac67a8bc5363641cc3218fddf2da46
lee910814 <wantto13>
2023-05-18 19:56:12 +0900

    add: img

---
+++ src/components/home/HomeSection.tsx
@@ -0,0 +1,30 @@
+import home from '@/components/Home/Home.module.scss';
+import Slider from "react-slick";
+
+
+// 여기 파일에서 작업해주세요
+const HomeSection = () => {
+  const settings = {
+    dots: true,
+    arrows: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: true,
+  };
+
+  return (
+    <section className={home['HomeSection']}>
+      <Slider {...settings}>
+   <img src="../img/fitta-cutout.png"/>
+     <img src=""/>
+       <img src=""/>
+ </Slider>
+    </section>
+  );
+};
+
+export default HomeSection;
