1b009a9ad3fe0745c9d92e3fafdc667bb7502c51
yiminwook <grs0416@gmail.com>
2023-05-31 20:12:27 +0900

    Update: separate top and bottom header nav

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,19 +5,25 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <header className={layout['header']}>
-      <div>
+      <div className={layout['headerInner']}>
+        <div>
+          <div>
+            <Link to="/">
+              <div className={layout['logo']}>
+                <span>logo</span>
+              </div>
+            </Link>
+            <ul>
+              <NavChild to="/signin" content="로그인" className={layout['signInButton']} />
+            </ul>
+          </div>
+        </div>
         <nav>
-          <Link to="/">
-            <div className={layout['logo']}>
-              <span>logo</span>
-            </div>
-          </Link>
           <ul>
             {/* pageLink 임시 */}
             <NavChild to="/search" content="검색" />
             <NavChild to="/admin" content="Admin" />
             <NavChild to="/signup" content="가입" />
-            <NavChild to="/signin" content="로그인" className={layout['signInButton']} />
           </ul>
         </nav>
       </div>
