ed2a1ff5562141eaa6d1305d84b58b1598c5839d
yiminwook <grs0416@gmail.com>
2023-06-09 18:51:13 +0900

    Update: sidebar style

--- src/components/layout/header/Sidebar.tsx
+++ src/components/layout/header/Sidebar.tsx
@@ -1,28 +1,30 @@
 import NavChild from '@/components/common/NavChild';
 import sidebar from '@/components/layout/header/Sidebar.module.scss';
 import DarkMode, { DarkModeProps } from '@/components/layout/header/DarkMode';
+import MenuProfile from '@/components/layout/header/MenuProfile';
+import { useUser } from '@/hooks/useUser';
 
 interface SidebarProps extends DarkModeProps {
-  signOut: () => void;
+  closeSidebar: () => void;
 }
 
-const Sidebar = ({ isDarkMode, setIsDarkMode, signOut }: SidebarProps) => {
+const Sidebar = ({ isDarkMode, setIsDarkMode, closeSidebar }: SidebarProps) => {
+  const { data: myData } = useUser();
+
   return (
     <aside className={sidebar['sidebar']}>
       <header>
         <h1 className="blind">사이드바</h1>
         <DarkMode isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
       </header>
+      {myData ? <MenuProfile /> : null}
       <ul>
         {/* pageLink 임시 */}
-        <NavChild to="/search" content="검색" />
-        <NavChild to="/owner/123" content="오너" />
-        <NavChild to="/signup" content="가입" />
-        <NavChild to="/signin" content="로그인" />
+        <NavChild to="/" content="홈" onClick={closeSidebar} />
+        {!myData ? <NavChild to="/signin" content="로그인" onClick={closeSidebar} /> : null}
+        <NavChild to="/search" content="검색" onClick={closeSidebar} />
+        <NavChild to="/signup" content="가입" onClick={closeSidebar} />
       </ul>
-      <footer>
-        <button onClick={signOut}>로그아웃</button>
-      </footer>
     </aside>
   );
 };
