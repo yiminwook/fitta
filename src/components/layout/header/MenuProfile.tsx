0a15c6bcf79d4d08637fe2adb4e624acda5fb6ca
yiminwook <grs0416@gmail.com>
2023-06-09 18:52:08 +0900

    Add: MenuProfile

---
+++ src/components/layout/header/MenuProfile.tsx
@@ -0,0 +1,24 @@
+import { useUser } from '@/hooks/useUser';
+import profile from '@/components/layout/header/Profile.module.scss';
+import { createDefaultProfileImage } from '@/utils/createDefaultProfileImage';
+import { useSignOut } from '@/hooks/useSignOut';
+
+interface MenuProfileProps {}
+
+const MenuProfile = ({}: MenuProfileProps) => {
+  const { data: myData } = useUser();
+  const { signOut } = useSignOut();
+  return (
+    <div className={profile['menuProfile']}>
+      <div>
+        <img src={createDefaultProfileImage({ key: myData!.name, size: '50px' })} alt="user-profile-image" />
+      </div>
+      <p>{myData?.name}</p>
+      <footer>
+        <button onClick={signOut}>로그아웃</button>
+      </footer>
+    </div>
+  );
+};
+
+export default MenuProfile;
