88e8881c9f5d4b8ae921d61da52f0749ee6aa4c4
yiminwook <grs0416@gmail.com>
2023-06-17 20:36:52 +0900

    Add: pagination components

---
+++ src/components/common/pagination/Pagination.tsx
@@ -0,0 +1,46 @@
+import { PAGINATION_SIZE } from '@/consts';
+import { useMemo } from 'react';
+import { useLocation, useSearchParams } from 'react-router-dom';
+import pagination from '@/components/common/pagination/Pagination.module.scss';
+import {
+  AfterLink,
+  BeforeLink,
+  FirstLink,
+  LastLink,
+  PaginationChild,
+} from '@/components/common/pagination/PaginationChild';
+
+interface PaginationProps {
+  totalPage: number;
+}
+
+const Pagination = ({ totalPage }: PaginationProps) => {
+  const { pathname } = useLocation();
+  const [searchParams] = useSearchParams();
+
+  const currentPage = useMemo(() => {
+    return Number(searchParams.get('page')) || 1;
+  }, [searchParams]);
+
+  let init = (Math.ceil(currentPage / PAGINATION_SIZE) - 1) * PAGINATION_SIZE + 1;
+  if (init <= 0) init = 1;
+
+  const pageArray = Array.from(Array(PAGINATION_SIZE), (x, i) => {
+    const page = i + init;
+    return page > totalPage ? null : page;
+  });
+
+  return (
+    <ul className={pagination['pagination']}>
+      <FirstLink currentPage={currentPage} />
+      <BeforeLink initialPage={pageArray[0]} totalPage={totalPage} />
+      {pageArray.map((page, index) => (
+        <PaginationChild key={`${pathname}-pagination-list-${index}`} page={page} currentPage={currentPage} />
+      ))}
+      <AfterLink initialPage={pageArray[0]} totalPage={totalPage} />
+      <LastLink currentPage={currentPage} totalPage={totalPage} />
+    </ul>
+  );
+};
+
+export default Pagination;
