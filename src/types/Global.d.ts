e2e99c3850dc25e466bb5e3e3ce96f48f305084b
yiminwook <grs0416@gmail.com>
2023-06-18 22:06:45 +0900

    Update: join Global.d.ts react-app-env.d.ts

--- src/types/Global.d.ts
+++ src/types/Global.d.ts
@@ -1,2 +1,16 @@
 declare module '*.module.css';
 declare module '*.module.scss';
+
+/// <reference types="react-scripts" />
+
+//override
+declare namespace NodeJS {
+  interface ProcessEnv {
+    readonly NODE_ENV: 'development' | 'production';
+    readonly REACT_APP_SITE_URL: string;
+    readonly REACT_APP_SERVER_URL: string;
+    readonly REACT_APP_KAKAO_JAVASCRIPT_KEY: string;
+    readonly REACT_APP_GOOGLE_API_KEY: string;
+    readonly REACT_APP_BUSINESS_NUMBER_API_KEY: string;
+  }
+}
