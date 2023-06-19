771ce10fa2facf0a78a5556c518cd706582c1b95
yiminwook <grs0416@gmail.com>
2023-06-19 22:29:23 +0900

    Add: Accordion component

--- src/components/member/Sidebar.tsx
+++ src/components/member/Sidebar.tsx
@@ -2,6 +2,7 @@ import NavChild from '@/components/common/NavChild';
 import member from '@/components/member/Member.module.scss';
 import { useUser } from '@/hooks/useAPI';
 import { Link } from 'react-router-dom';
+import Accordion from '@/components/common/accordion/Accordion';
 
 const MemberSidebar = () => {
   const { myData } = useUser();
@@ -14,10 +15,11 @@ const MemberSidebar = () => {
     <aside className={member['sidebar']}>
       <h1 className="blind">멤버 사이드바</h1>
       <Link to={`/member/${myData!.id}/home`}>마이페이지(멤버)</Link>
-      <ul>
-        <NavChild to={`/member/${myData!.id}/home`} content="마이페이지" />
-        <NavChild to={`/member/${myData!.id}/gym`} content="헬스장" />
-      </ul>
+      <Accordion title="헬스장">
+        <ul>
+          <NavChild to={`/member/${myData!.id}/gym`} content="헬스장 현황" />
+        </ul>
+      </Accordion>
     </aside>
   );
 };
--- src/components/owner/Sidebar.tsx
+++ src/components/owner/Sidebar.tsx
@@ -2,6 +2,7 @@ import NavChild from '@/components/common/NavChild';
 import owner from '@/components/owner/Owner.module.scss';
 import { useUser } from '@/hooks/useAPI';
 import { Link } from 'react-router-dom';
+import Accordion from '@/components/common/accordion/Accordion';
 
 const OwnerSidebar = () => {
   const { myData } = useUser();
@@ -14,13 +15,23 @@ const OwnerSidebar = () => {
     <aside className={owner['sidebar']}>
       <h1 className="blind">오너 사이드바</h1>
       <Link to={`/owner/${myData!.id}/home`}>마이페이지(오너)</Link>
-      <ul>
-        <NavChild to={`/owner/${myData!.id}/gym`} content="헬스장" />
-        <NavChild to={`/owner/${myData!.id}/staff`} content="스태프 현황" />
-        <NavChild to={`/owner/${myData!.id}/member`} content="멤버 현황" />
-        <NavChild to={`/owner/${myData!.id}/staff/edit`} content="스태프 관리" />
-        <NavChild to={`/owner/${myData!.id}/member/edit`} content="멤버 관리" />
-      </ul>
+      <Accordion title="헬스장">
+        <ul>
+          <NavChild to={`/owner/${myData!.id}/gym`} content="헬스장 현황" />
+        </ul>
+      </Accordion>
+      <Accordion title="스태프">
+        <ul>
+          <NavChild to={`/owner/${myData!.id}/staff`} content="스태프 현황" />
+          <NavChild to={`/owner/${myData!.id}/staff/edit`} content="스태프 관리" />
+        </ul>
+      </Accordion>
+      <Accordion title="멤버">
+        <ul>
+          <NavChild to={`/owner/${myData!.id}/member`} content="멤버 현황" />
+          <NavChild to={`/owner/${myData!.id}/member/edit`} content="멤버 관리" />
+        </ul>
+      </Accordion>
     </aside>
   );
 };
