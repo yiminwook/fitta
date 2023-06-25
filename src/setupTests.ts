4abb09f0702824203db8ee2d7bb5b209ca867827
yiminwook <grs0416@gmail.com>
2023-06-26 00:07:25 +0900

    Update: jest setUp window.matchMedia, alias and add jest-canvas-mock

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,15 +1,28 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import 'jest-canvas-mock';
 import mockServer from '@/mocks/server';
 
 //테스트 전에 1번 실행
 beforeAll(() => {
   mockServer.listen({ onUnhandledRequest: 'bypass' });
+  Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation((query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // Deprecated
+      removeListener: jest.fn(), // Deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })),
+  });
 });
+
 //테스트가 끝난후
 afterEach(() => mockServer.resetHandlers());
 //모든 테스트가 끝난후 1번 실행
 afterAll(() => mockServer.close());
+
+// const url = screen.logTestingPlaygroundURL();
