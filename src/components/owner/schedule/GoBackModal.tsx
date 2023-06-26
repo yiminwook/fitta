9aa17a81fe1676ea031ea04b556a441971119c73
yiminwook <grs0416@gmail.com>
2023-06-26 21:12:47 +0900

    Add: GoBackModal component

--- src/components/owner/schedule/GoBackModal.tsx
+++ src/components/owner/schedule/GoBackModal.tsx
@@ -14,7 +14,8 @@ const GoBackModal = () => {
   };
 
   return (
-    <Modal title="나가시겠습니까?" onClose={onClose}>
+    <Modal title="주의" onClose={onClose}>
+      <h2>작업중인 정보를 잃을 수 있습니다.</h2>
       <div>
         <button onClick={goBack}>나가기</button>
       </div>
