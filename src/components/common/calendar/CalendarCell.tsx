f211c7db58ef64505ee27ce9fbe64cc6ceda99fe
yiminwook <grs0416@gmail.com>
2023-06-26 21:12:01 +0900

    Add: calendar style

--- src/components/common/calendar/CalendarCell.tsx
+++ src/components/common/calendar/CalendarCell.tsx
@@ -1,4 +1,6 @@
 import { CalendarCellType } from '@/components/common/calendar/Calendar';
+import { memo, useMemo } from 'react';
+import calendar from '@/components/common/calendar/Calendar.module.scss';
 
 interface CalendarCellProps {
   cell: CalendarCellType;
@@ -6,11 +8,41 @@ interface CalendarCellProps {
   colIndex: number;
   nowYear: number;
   nowMonth: number;
+  selected: string[];
+  toggleSelect: (date: string) => void;
 }
 
-const CalendarCell = ({ cell, rowIndex, colIndex, nowYear, nowMonth }: CalendarCellProps) => {
-  const { month, date } = cell;
-  return <td>{`${month} / ${date}`}</td>;
+const CalendarCell = ({ cell, rowIndex, colIndex, nowYear, nowMonth, selected, toggleSelect }: CalendarCellProps) => {
+  const { date } = cell;
+
+  const format = useMemo(() => date.format('YYYY-MM-DD'), [date]);
+
+  const onClick = () => {
+    toggleSelect(format);
+  };
+
+  const setClassName = useMemo(() => {
+    let classNames: any[] = [];
+    if (nowMonth !== date.get('month') + 1) {
+      classNames.push(calendar['surround']);
+    }
+
+    if (selected.includes(format)) {
+      classNames.push(calendar['selected']);
+    }
+
+    if (selected[0] === format) {
+      classNames.push(calendar['start']);
+    }
+
+    if (selected[selected.length - 1] === format) {
+      classNames.push(calendar['end']);
+    }
+
+    return classNames.join(' ');
+  }, [selected, date, nowMonth, format]);
+
+  return <td onClick={onClick} className={setClassName}>{`${date.get('date')}`}</td>;
 };
 
-export default CalendarCell;
+export default memo(CalendarCell);
